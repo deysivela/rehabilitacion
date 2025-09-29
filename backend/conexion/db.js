73df448f57345f4639fea67b1c86d5eb45ad6c29
Deysi Vela Estrada <deysivelaestrada@gmail.com>
2025-09-29 17:08:25 -0400

    db

--- backend/conexion/db.js
+++ backend/conexion/db.js
@@ -1,10 +1,18 @@
-
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('rehabilitacion', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
+
 sequelize.authenticate()
   .then(() => console.log('Conexión establecida con la base de datos'))
   .catch((error) => console.error('Error al conectar:', error));
+
 module.exports = sequelize;
