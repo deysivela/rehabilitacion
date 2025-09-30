ec112718830b07d656dff376967bcbe7bdd5d3e2
Deysi Vela Estrada <deysivelaestrada@gmail.com>
2025-09-29 20:41:27 -0400

    base

--- backend/conexion/db.js
+++ backend/conexion/db.js
@@ -1,18 +1,16 @@
-const { Sequelize } = require('sequelize');
-
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        ca: require('fs').readFileSync(process.env.DB_SSL_CA)
+      }
+    },
     logging: false,
   }
 );
-
-sequelize.authenticate()
-  .then(() => console.log('Conexión establecida con la base de datos'))
-  .catch((error) => console.error('Error al conectar:', error));
-
-module.exports = sequelize;
