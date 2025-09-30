50b6814f2e247331b6ef418877310eb7a4e6a011
Deysi Vela Estrada <deysivelaestrada@gmail.com>
2025-09-29 21:42:16 -0400

    my

--- backend/rutas/usuario.js
+++ backend/rutas/usuario.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 //const bcrypt = require('bcrypt');
 const bcrypt = require('bcryptjs');
-const Usuario = require('../modelos/usuario');
+const Usuario = require('../modelos/Usuario');
 
 // Listar usuarios
 router.get('/listar', async (req, res) => {
