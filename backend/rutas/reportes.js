a7973d4c5dbdd6188b0e9ad52bef263447ab57c3
Deysi Vela Estrada <deysivelaestrada@gmail.com>
2025-10-02 08:03:49 -0400

    exc

--- backend/rutas/reportes.js
+++ backend/rutas/reportes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { generarExcelReporte } = require("../servicios/excel");
+const { generarExcelReporte } = require("../../frontend/src/servicios/excel");
 const db = require("../modelos");
 const { Op } = require("sequelize");
 
